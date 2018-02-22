9ee48790cc001979335f9f2c5268202649b71c91
semtax <tlqaksqhr@naver.com>
2018-02-22 15:41:07 +0900

    2018.02.22 chapter 21 ended.. :D

---
+++ SourceCodeBackUp/Chap21/02.Kernel64/Source/AssemblyUtility.h
@@ -0,0 +1,22 @@
+#ifndef __ASSEMBLYUTILITY_H__
+#define __ASSEMBLYUTILITY_H__
+
+#include "Types.h"
+#include "Task.h"
+
+
+BYTE kInPortByte( WORD wPort );
+void kOutPortByte( WORD wPort, BYTE bData );
+void kLoadGDTR( QWORD qwGDTRAddress );
+void kLoadTR( WORD wTSSSegmentOffset );
+void kLoadIDTR( QWORD qwIDTRAddress);
+void kEnableInterrupt( void );
+void kDisableInterrupt( void );
+QWORD kReadRFLAGS( void );
+QWORD kReadTSC( void );
+void kSwitchContext( CONTEXT* pstCurrentContext, CONTEXT* pstNextContext );
+void kHlt(void);
+
+BOOL kTestAndSet(volatile BYTE *pbDestination,BYTE bCompare,BYTE bSource);
+
+#endif
