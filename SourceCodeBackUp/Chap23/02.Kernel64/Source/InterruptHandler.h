01b5b249c45d7d178f3fcfb67654ba095cfec046
semtax <tlqaksqhr@naver.com>
2018-02-25 20:28:08 +0900

    Memory Management routine added(Buddy heap allocator)

---
+++ SourceCodeBackUp/Chap23/02.Kernel64/Source/InterruptHandler.h
@@ -0,0 +1,18 @@
+#ifndef __INTERRUPTHANDLER_H__
+#define __INTERRUPTHANDLER_H__
+
+#include "Types.h"
+#include "PIC.h"
+#include "Keyboard.h"
+#include "Console.h"
+#include "Utility.h"
+#include "Task.h"
+#include "Descriptor.h"
+
+void kCommonExceptionHandler(int iVectorNumber,QWORD qwErrorCode);
+void kCommonInterruptHandler(int iVectorNumber);
+void kKeyboardHandler(int iVectorNumber);
+void kTimerHandler(int iVectorNumber);
+void KDeviceNotAvailableHandler(int iVectorNumber);
+
+#endif
